764a5d0a45456eb23810b15312a006b6862c1b69
althaffz <althaffaiz@gmail.com>
2021-04-04 17:41:02 -0400

    GUI Rendered

--- app.js
+++ app.js
@@ -2,27 +2,50 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
 
-const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-//const city = "London";
 const api_key = "8c167268a15674e7b643046030455fa3";
 const unit = "metric";
 
 
-
-
-
 app.get('/', function(req, res){
-  res.sendFile(__dirname + "/post.html");
+  var city = "Toronto";
+  const url = "https://api.openweathermap.org/data/2.5/weather?q=" +city+ "&appid="+api_key+"&units=" + unit;
+  https.get(url, function(response){
+    response.on('data', function(data){
+      const weatherData = JSON.parse(data);
+
+      var d = new Date();
+      var time = ""+ d.getHours() +":"+d.getMinutes();
+      var today = "" + d.getDate() +"/" + d.getMonth() +"/" + d.getFullYear();
+      console.log(today);
+
+      if (weatherData.cod == 200){
+        var tempData = {
+          _city: city,
+          _time: time,
+          _temp: weatherData.main.temp,
+          _weather: weatherData.weather[0].description,
+          _windSpeed: weatherData.wind.speed,
+          _humidity: weatherData.main.humidity,
+          _icon: weatherData.weather[0].icon,
+          _today: today,
+          _message: ""
+        }
+
+        res.render('weather', tempData);
+      }
+    });
+  });
 });
 
+
+
 app.post('/', function(req, res){
-  const city = req.body.city;
+  const city = req.body.city_name;
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" +city+ "&appid="+api_key+"&units=" + unit;
   https.get(url, function(response){
     response.on('data', function(data){
@@ -40,6 +63,8 @@ app.post('/', function(req, res){
       var today = "" + nd.getDate() +"/" + nd.getMonth() +"/" + nd.getFullYear();
       console.log(today);
 
+      var message = "";
+
       if (weatherData.cod == 200){
         var tempData = {
           _city: city,
@@ -49,7 +74,8 @@ app.post('/', function(req, res){
           _windSpeed: weatherData.wind.speed,
           _humidity: weatherData.main.humidity,
           _icon: weatherData.weather[0].icon,
-          _today: today
+          _today: today,
+          _message: message
         }
 
         res.render('weather', tempData);
